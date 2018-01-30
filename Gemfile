c89bd4a6634d0ca80a9f9fd7fbd36a4fa84debdb
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-01-30 14:03:54 +0000

    HUGE topics checkpoint commit. Added topics element as well as associations between topics and posts

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.0'
   gem 'sqlite3'
+  gem 'shoulda'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 require 'random_data'
 
+#Create Topics
+15.times do
+    Topic.create!(
+        name: RandomData.random_sentence,
+        description: RandomData.random_paragraph
+    )
+end
+topics = Topic.all
+
 #Create posts
 50.times do
     Post.create!(
+        topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
     )
@@ -32,5 +42,6 @@ Post.find_or_create_by(title: "example title", body: "example body")
 puts "#{Post.count}"
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
