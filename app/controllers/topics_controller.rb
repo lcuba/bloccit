aa6743f07fae10a87cb74e1937c4e011d16d70b3
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-02-04 22:34:36 +0000

    attempt to add moderator role to bloccit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,9 @@
 class TopicsController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :require_sign_in, except: [:index, :show]
-    before_action :authorize_user, except: [:index, :show]
+    before_action :authorize_user_to_edit, except: [:index, :show, :new, :create, :destroy]
+    before_action :authorize_user_to_create_or_delete, except: [:index, :show, :edit, :update]
+    
     def index
         @topics = Topic.all
     end
@@ -59,9 +61,16 @@ class TopicsController < ApplicationController
         params.require(:topic).permit(:name, :description, :public)
     end
     
-    def authorize_user
+    def authorize_user_to_create_or_delete
         unless current_user.admin?
             flash[:alert] = "You must be an admin to do that."
+            redirect_to topics_path
+        end
+    end
+    
+    def authorize_user_to_edit
+        unless current_user.admin? || current_user.moderator?
+            flash[:alert] = "You must have admin or moderator privileges to do that."
             redirect_to topics_path
         end
     end
