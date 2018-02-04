7993de163e19f5a22450d6c259ba70283cc4771d
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-02-04 21:31:57 +0000

    Added authorizations to CRUD operations on posts and topics. Three roles, guest, member, and admin.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203063134) do
+ActiveRecord::Schema.define(version: 20180204203415) do
 
   create_table "advertisments", force: :cascade do |t|
     t.string "title"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20180203063134) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -52,10 +52,19 @@ puts "#{Post.count}"
 Post.find_or_create_by(title: "example title", body: "example body")
 puts "#{Post.count}"
 
-user = User.first
-user.update_attributes!(
-    email: 'lucas.cuba4@gmail.com',
-    password: 'c6h12o65'
+# Create an admin user
+admin = User.create!(
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: 'helloworld',
+    role: 'admin'
+)
+
+# Create a member
+member = User.create!(
+    name: 'Member User',
+    email: 'member@example.com',
+    password: 'helloworld'
 )
 
 puts "Seed finished"
