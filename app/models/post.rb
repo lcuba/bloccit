7670f307504ad69f9934a8bc565dfe5306c515e9
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-02-06 05:59:28 +0000

    voting checkpoint work. Added ability to vote on posts and also have posts be sorted by this upvote/downvote value. Also implemented a time decay algorithm for ranked posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
     belongs_to :topic
     belongs_to :user
     has_many :comments, dependent: :destroy
-    default_scope {order('created_at DESC')}
+    has_many :votes, dependent: :destroy
+    default_scope {order('rank DESC')}
 #    scope :ordered_by_title {order('title DESC')}
 #    scope :ordered_by_rerverse_created_at {order('created_at ASC')}
     
@@ -10,4 +11,23 @@ class Post < ApplicationRecord
     validates :body, length: {minimum: 20}, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+    
+    def up_votes
+        votes.where(value: 1).count
+    end
+    
+    def down_votes
+        votes.where(value: -1).count
+    end
+    
+    def points
+        votes.sum(:value)
+    end
+    
+    def update_rank
+       age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+       new_rank = points + age_in_days
+       update_attribute(:rank, new_rank)
+    end
+
 end
