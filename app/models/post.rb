bb0917583f2b4404074130189c0bb254a97a3c9d
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-02-06 06:04:17 +0000

    small assignment addition to post creation that automatically upvotes a user's post when they create it

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ApplicationRecord
     default_scope {order('rank DESC')}
 #    scope :ordered_by_title {order('title DESC')}
 #    scope :ordered_by_rerverse_created_at {order('created_at ASC')}
+
+    after_create :create_vote
     
     validates :title, length: {minimum: 5}, presence: true
     validates :body, length: {minimum: 20}, presence: true
@@ -28,6 +30,12 @@ class Post < ApplicationRecord
        age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
        new_rank = points + age_in_days
        update_attribute(:rank, new_rank)
+    end
+    
+    private
+    
+    def create_vote
+        user.votes.create(value: 1, post: self)
     end
 
 end
