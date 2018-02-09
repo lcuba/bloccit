a82e81362584cb9fd82e884735e4b28409a9cba9
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-02-09 07:49:52 +0000

    Another attempt at the public-profiles assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def posts?(user)
+        user.posts.any?
+    end
+    
+    def comments?(user)
+        user.comments.any? 
+    end
+    
+    def favorites?(user)
+        user.favorites.any?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
     end
     
     def avatar_url(size)
-       gravatar_id = Digest::MD5::hexdigest(self.email).downcase
-       "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+        gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     end
 end
