5b44c11ddbd8d8e59f1887182b04c6ea4d280686
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-05-07 01:36:21 +0000

    refactor, renaming, and update to deploy to heroku

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+ruby '2.4.1'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
@@ -62,7 +64,8 @@ group :development do
 end
 
 group :production do
-  gem 'pg', '~> 0.20'
+  gem 'pg'
+  gem 'rails_12factor'
 end
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  config.action_mailer.raise_delivery_errors = false
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://lcuba-readdit.herokuapp.com/' }
 end
